320ce597d3376fc7b85dd253893bc014923d87f5
r-bytes <101632683+r-bytes@users.noreply.github.com>
2022-11-01 16:49:41 +0100

    fix: naming convention

--- next.config.js
+++ next.config.js
@@ -3,12 +3,12 @@ module.exports = {
     reactStrictMode: true,
     images: {
         remotePatterns: [
-        {
-            protocol: 'https',
-            hostname: 'cdn.sanity.io',
-            port: '',
-            pathname: '/images/**',
-        },
+            {
+                protocol: 'https',
+                hostname: 'cdn.sanity.io',
+                port: '',
+                pathname: '/images/**',
+            },
         ],
     },
 };
--- sanity.js
+++ sanity.js
@@ -6,8 +6,8 @@ import {
 import createImageUrlBuilder from '@sanity/image-url'
 
 const config = {
-    dataset: process.env.PUBLIC_NEXT_SANITY_DATASET || "production",
-    projectId: process.env.PUBLIC_NEXT_SANITY_PROJECT_ID || "le9t2xfy",
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
     apiVersion: "2021-10-21",
     useCdn: process.env.NODE_ENV === "production"
 }

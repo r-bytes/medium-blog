828ad35f2e86a97915ddaf061fe5f04c2f5e49ee
r-bytes <101632683+r-bytes@users.noreply.github.com>
2022-11-01 15:22:40 +0100

    feat: sanity functionality

---
+++ sanity.js
@@ -0,0 +1,22 @@
+import {
+    createCurrentUserHook,
+    createClient,
+} from "next-sanity"
+
+import createImageUrlBuilder from '@sanity/image-url'
+
+const config = {
+    dataset: process.env.PUBLIC_NEXT_SANITY_DATASET || "production",
+    projectId: process.env.PUBLIC_NEXT_SANITY_PROJECT_ID || "le9t2xfy",
+    apiVersion: "2021-10-21",
+    useCdn: process.env.NODE_ENV === "production"
+}
+
+// set up the client for fetching data in the getProps page functions
+export const sanityClient = createClient(config)
+
+// helper function for generating Image Urls with only the asset reference data in you documents https://www.sanity.io/docs/image-url
+export const urlFor = (source) => createImageUrlBuilder(config).image(source)
+
+// helper function for using the current logged in user account
+export const useCurrentUser = createCurrentUserHook(config)

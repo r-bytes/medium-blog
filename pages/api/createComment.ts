2dcfbd3b8b2694e313be2c946e29cdc38a68d290
r-bytes <101632683+r-bytes@users.noreply.github.com>
2022-11-02 20:37:43 +0100

    feat: create comment api

---
+++ pages/api/createComment.ts
@@ -0,0 +1,15 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+const config = {
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    useCdn: process.env.NODE_ENV === "production",
+    token: process.env.NEXT_PUBLIC_SANITY_API_TOKEN,
+}
+
+export default function createComment(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    res.status(200).json({ name: "John Doe" });
+}

d1a6a0c6828ffac91c1be350fe01234c427f2b0a
r-bytes <101632683+r-bytes@users.noreply.github.com>
2022-11-03 15:19:11 +0100

    feat: api to create new comment in sanity

--- pages/api/createComment.ts
+++ pages/api/createComment.ts
@@ -1,15 +1,37 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import sanityClient from "@sanity/client";
 
 const config = {
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    apiVersion: '2021-08-31',
     useCdn: process.env.NODE_ENV === "production",
-    token: process.env.NEXT_PUBLIC_SANITY_API_TOKEN,
+    token: process.env.SANITY_API_TOKEN,
 }
+const client = sanityClient(config)
 
-export default function createComment(
+export default async function createComment(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
-    res.status(200).json({ name: "John Doe" });
+    const { _id, name, email, comment } = JSON.parse(req.body)
+
+    try {
+        await client.create({
+            _type: "comment",
+            post: {
+                _type: "reference",
+                _ref: _id,
+            },
+            name,
+            email,
+            comment,
+        })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ message: "Couldn't submit comment", error})
+    }
+
+    console.log("Comment submitted")
+    return res.status(200).json({ message: "Comment submitted" });
 }
